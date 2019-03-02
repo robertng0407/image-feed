ca8373b7076f79f2683ac9c5565286dfa380286d
robertng0407 <robertng0407@gmail.com>
2019-03-02 14:37:58 -0800

    Add avatar component

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { Constants } from 'expo';
+
+import Avatar from './src/components/Avatar';
+
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Avatar
+          initials="RN"
+          size={35}
+          backgroundColor={'teal'}
+        />
       </View>
     );
   }
@@ -15,7 +23,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    marginTop: Constants.statusBarHeight
   },
 });
---
+++ src/components/Avatar.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { View, Text, StyleSheet, ColorPropType } from 'react-native';
+import PropTypes from 'prop-types';
+
+export default function Avatar({ initials, size, backgroundColor }) {
+    const style = {
+        width: size,
+        height: size,
+        borderRadius: size / 2,
+        backgroundColor
+    }
+    return (
+        <View style={[style, styles.container]}>
+            <Text style={styles.text}>{initials}</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text: {
+        color: 'white'
+    }
+});
+
+Avatar.propTypes = {
+    initials: PropTypes.string.isRequired,
+    size: PropTypes.number.isRequired,
+    backgroundColor: ColorPropType.isRequired
+}
