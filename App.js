9878bd6809e6ad3cf65fe3cf0e48f3b1d605569e
robertng0407 <robertng0407@gmail.com>
2019-03-05 21:49:37 -0800

    Add feed screen

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Platform } from 'react-native';
 
 import { Constants } from 'expo';
 
-import CardList from './src/components/CardList';
+import Feed from './src/screens/Feed';
 
 const items = [
   { id: 0, author: 'Rob' },
@@ -14,17 +14,28 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <CardList items={items} />
+        <Feed style={styles.feed} />
       </View>
     );
   }
 }
 
+const platformVersion =
+  Platform.OS === 'ios'
+    ? parseInt(Platform.Version, 10)
+    : Platform.version;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    justifyContent: 'flex-start',
-    marginTop: Constants.statusBarHeight
+    justifyContent: 'flex-start'
   },
+  feed: {
+    flex: 1,
+    marginTop:
+      Platform.OS === 'android' || platformVersion < 11
+        ? Constants.statusBarHeight
+        : 0
+  }
 });
